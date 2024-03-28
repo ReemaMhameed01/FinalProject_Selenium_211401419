580411e9b16544e96b7970d86a2e2107bf949fd9
Reema <Reema@10.0.0.20>
2024-03-28 15:05:57 +0200

    First Ques(Check Password) - Sellenium

---
+++ src/test/java/Selleniun_start_projects/Selleniun_start_projects/CheckPass.java
@@ -0,0 +1,83 @@
+package Selleniun_start_projects.Selleniun_start_projects;
+
+import static org.junit.Assert.assertNotEquals;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class CheckPass {
+	static WebDriver  browser;
+
+	@Before
+	public void setup() {
+
+		System.setProperty("webdriver.gecko.driver","C:\\selenium\\geckodriver.exe");
+		browser = new FirefoxDriver();
+		browser.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
+		String url="https://testpages.eviltester.com/styled/apps/7charval/simple7charvalidation.html";
+		browser.get(url);
+	}
+
+	@Test
+	public void CheckCorrectPassword() {
+		browser.findElement(By.name("characters")).sendKeys("Reema12");
+		 browser.findElement(By.name("validate")).click();
+		 String expectedResult = "Valid Value";
+		 String result= browser.findElements(By.name("validation_message")).toString();
+		 assertNotEquals(result ,expectedResult);
+		 browser.close();
+	}
+	@Test
+	public void CheckShortPassword() {
+		browser.findElement(By.name("characters")).sendKeys("reema");
+		 browser.findElement(By.name("validate")).click();
+		 String expectedResult = "Invalid Value";
+		 String result= browser.findElements(By.name("validation_message")).toString();
+		 assertNotEquals(result ,expectedResult);
+		 browser.close();
+	}
+	@Test
+	public void CheckLongPassword() {
+		browser.findElement(By.name("characters")).sendKeys("mhameedreema123");
+		 browser.findElement(By.name("validate")).click();
+		 String expectedResult = "Invalid Value";
+		 String result= browser.findElements(By.name("validation_message")).toString();
+		 assertNotEquals(result ,expectedResult);
+		 browser.close();
+	}
+	@Test
+	public void CheckPasswordWithChar() {
+		browser.findElement(By.name("characters")).sendKeys("Rima#12");
+		 browser.findElement(By.name("validate")).click();
+		 String expectedResult = "Invalid Value";
+		 String result= browser.findElements(By.name("validation_message")).toString();
+		 assertNotEquals(result ,expectedResult);
+		 browser.close();
+	}
+	@Test
+	public void CheckPasswordWithChar2() {
+		browser.findElement(By.name("characters")).sendKeys("Rima!t4");
+		 browser.findElement(By.name("validate")).click();
+		 String expectedResult = "Invalid Value";
+		 String result= browser.findElements(By.name("validation_message")).toString();
+		 assertNotEquals(result ,expectedResult);
+		 browser.close();
+	}
+	@Test
+	public void CheckPasswordWithChar3() {
+		browser.findElement(By.name("characters")).sendKeys("Inval)3");
+		 browser.findElement(By.name("validate")).click();
+		 String expectedResult = "Invalid Value";
+		 String result= browser.findElements(By.name("validation_message")).toString();
+		 assertNotEquals(result ,expectedResult);
+		 browser.close();
+	}
+	
+	
+
+}
